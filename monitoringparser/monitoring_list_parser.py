4d81a3098a43725d72210845f286098465a791d0
ArtemKondratenko <artem-osteo@yandex.ru>
2024-08-28 19:09:22 +0300

    Add method read_existing_path

--- monitoringparser/monitoring_list_parser.py
+++ monitoringparser/monitoring_list_parser.py
@@ -7,8 +7,24 @@ def load(file: TextIO) -> set[Path]:
     return set(Path(path.strip()) for path in file)
 
 
+
+
+def read_existing_paths(file_path: Path) -> set:
+    """Читает существующие пути из файла и возвращает их в виде множества."""
+    if file_path.exists():
+        with open(file_path, 'r') as file:
+            return set(file.read().strip().splitlines())
+    return set()
+
 def dump(file_path: Path, paths: Iterable[Path]):
-    with open(file_path, 'a+') as file:
+    """Записывает уникальные пути в файл."""
+    existing_paths = read_existing_paths(file_path)
+    with open(file_path, 'a') as file:
         for path in paths:
             absolute_path = Path(path).absolute()
-            file.write(str(absolute_path) + "\n")
+            if str(absolute_path) not in existing_paths:
+                file.write(str(absolute_path) + "\n")
+                print(f"Added: {absolute_path}")
+            else:
+                print(f"Already exists: {absolute_path}")
+
