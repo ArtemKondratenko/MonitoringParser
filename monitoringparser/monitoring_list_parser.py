ce6325e1fd4c08bf07e7f1dd5346025f6e15da36
ArtemKondratenko <artem-osteo@yandex.ru>
2024-08-28 18:47:43 +0300

    Update method dump

--- monitoringparser/monitoring_list_parser.py
+++ monitoringparser/monitoring_list_parser.py
@@ -7,7 +7,8 @@ def load(file: TextIO) -> set[Path]:
     return set(Path(path.strip()) for path in file)
 
 
-def dump(paths: Iterable[Path], file: TextIO):
-    for path in paths:
-        absolute_path = Path(path).absolute()
-        file.write(str(absolute_path) + "\n")
+def dump(file_path: Path, paths: Iterable[Path]):
+    with open(file_path, 'a') as file:
+        for path in paths:
+            absolute_path = Path(path).absolute()
+            file.write(str(absolute_path) + "\n")
